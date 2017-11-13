9403c49d9933d74601f602be652d90cc4638d16a
frank <yangxb@xinguangnet.com>
2017-11-13 17:57:37 +0800

    spring_could_Ribbon

--- eurekaclient-02/src/main/java/com/frank/Eurekaclient02Application.java
+++ eurekaclient-02/src/main/java/com/frank/Eurekaclient02Application.java
@@ -2,10 +2,12 @@ package com.frank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Eurekaclient02Application {
 
---
+++ eurekaclient-02/src/main/java/com/frank/TestController.java
@@ -0,0 +1,32 @@
+package com.frank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by yangxb on 2017/11/13.
+ */
+
+@RestController
+public class TestController {
+
+    @Value("${spring.application.name}")
+    String name;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return restTemplate.getForEntity("http://eurekaclient/add?a=10&b=20", String.class).getBody();
+    }
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String Hi() {
+        return restTemplate.getForEntity("http://eurekaclient/hi?name="+name, String.class).getBody();
+    }
+
+}
